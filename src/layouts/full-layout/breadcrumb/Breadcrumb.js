bed7d8a90beb13ed7f59d61d77f0efbf5ee8e6e2
helloljho <helloljho@gmail.com>
2022-01-26 10:38:24 +0900

    fix : button in header

--- src/layouts/full-layout/breadcrumb/Breadcrumb.js
+++ src/layouts/full-layout/breadcrumb/Breadcrumb.js
@@ -1,71 +1,141 @@
-import React from 'react';
-import { Grid, Typography, Box, Breadcrumbs, Link, Button } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Grid, Typography, Box, Breadcrumbs, Button } from '@mui/material';
+import { NavLink, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import AddIcon from '@mui/icons-material/Add';
 
-const Breadcrumb = ({ subtitle, items, title, children }) => (
-  <Grid
-    container
-    sx={{
-      p: '15px',
-    }}
-  >
-    <Grid item xs={6} sm={6} lg={8}>
-      <Typography color="textSecondary" fontWeight="400" variant="h4">
-        {subtitle}
-      </Typography>
-
-      <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
-        {items
-          ? items.map((item) => (
-              <div key={item.title}>
-                {item.to ? (
-                  <Link underline="none" color="inherit" component={NavLink} to={item.to}>
-                    {item.title}
-                  </Link>
-                ) : (
-                  <Typography color="textPrimary">{item.title}</Typography>
-                )}
-              </div>
-            ))
-          : ''}
-      </Breadcrumbs>
-      <Typography
-        fontWeight="700"
-        variant="h1"
-        sx={{
-          lineHeight: '1.235',
-        }}
-      >
-        {title}
-      </Typography>
-    </Grid>
+const Breadcrumb = ({ subtitle, items, title, children }) => {
+  return (
     <Grid
-      item
-      xs={6}
-      sm={6}
-      lg={4}
-      display="flex"
-      alignItems="flex-end"
-      style={{ border: '1px solid red' }}
+      container
+      sx={{
+        p: '15px',
+      }}
     >
-      <Box
-        sx={{
-          // display: { xs: 'none', md: 'flex', lg: 'flex' },
-          // alignItems: 'center',
-          // justifyContent: 'flex-end',
-          textAlign: 'right',
-          width: '100%',
-        }}
-      >
-        {children}
-        <Button variant="contained">asdf</Button>
-      </Box>
-    </Grid>
-  </Grid>
-);
+      <Grid item xs={6} sm={6} lg={8}>
+        <Typography color="textSecondary" fontWeight="400" variant="h4">
+          {subtitle}
+        </Typography>
 
+        <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+          {items
+            ? items.map((item) => (
+                <div key={item.title}>
+                  {item.to ? (
+                    <Link underline="none" color="inherit" component={NavLink} to={item.to}>
+                      {item.title}
+                    </Link>
+                  ) : (
+                    <Typography color="textPrimary">{item.title}</Typography>
+                  )}
+                </div>
+              ))
+            : ''}
+        </Breadcrumbs>
+        <Typography
+          fontWeight="700"
+          variant="h1"
+          sx={{
+            lineHeight: '1.235',
+          }}
+        >
+          {title}
+        </Typography>
+      </Grid>
+      <Grid item xs={6} sm={6} lg={4} display="flex" alignItems="flex-end">
+        <Box
+          sx={{
+            textAlign: 'right',
+            width: '100%',
+          }}
+        >
+          {/*{children}*/}
+          {/*{title.toLowerCase() === 'company' && (*/}
+          {/*  <Button variant="contained">*/}
+          {/*    <AddIcon />*/}
+          {/*    {t('Register Company')}*/}
+          {/*  </Button>*/}
+          {/*)}*/}
+          {/*{title.toLowerCase() === 'nfts' && <Button variant="contained">{t('NFT Mint')}</Button>}*/}
+          {/*{title.toLowerCase() === 'airdrop' && (*/}
+          {/*  <Button variant="contained">{t('AirDrop Mint')}</Button>*/}
+          {/*)}*/}
+          {/*{title.toLowerCase() === 'serials' && <Button variant="contained">{t('Create')}</Button>}*/}
+          {/*{title.toLowerCase() === 'collection' && (*/}
+          {/*  <Button variant="contained">{t('Create')}</Button>*/}
+          {/*)}*/}
+          {/*{title.toLowerCase() === 'reward' && <Button variant="contained">{t('Create')}</Button>}*/}
+          <LinkButton title={title} />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+const LinkButton = ({ title }) => {
+  const { t } = useTranslation();
+  const [buttonProps, setButtonProps] = useState({ label: null, ulr: null });
+
+  useEffect(() => {
+    switch (title.toLowerCase()) {
+      case 'company':
+        setButtonProps({
+          label: 'Register Company',
+          url: '',
+        });
+        break;
+      case 'nfts':
+        setButtonProps({
+          label: 'NFT Mint',
+          url: '/nfts/mint',
+        });
+        break;
+      case 'airdrop':
+        setButtonProps({
+          label: 'AirDrop Mint',
+          url: '',
+        });
+        break;
+      case 'serials':
+        setButtonProps({
+          label: 'Create',
+          url: '',
+        });
+        break;
+      case 'collection':
+        setButtonProps({
+          label: 'Create',
+          url: '',
+        });
+        break;
+      case 'reward':
+        setButtonProps({
+          label: 'Create',
+          url: '',
+        });
+        break;
+    }
+  }, [title]);
+
+  return (
+    <>
+      {buttonProps.label !== null && (
+        // <Button variant="contained">
+        //   <AddIcon style={{ marginRight: '5px' }} />
+        //   <Link to={buttonProps.url}>{t(`${buttonProps.label}`)}</Link>
+        // </Button>
+
+        <Button variant="contained" component={Link} to={{ pathname: buttonProps.url }}>
+          <AddIcon style={{ marginRight: '5px' }} />
+          {t(`${buttonProps.label}`)}
+          {/*<Link to={buttonProps.url}>{t(`${buttonProps.label}`)}</Link>*/}
+        </Button>
+      )}
+    </>
+  );
+};
 Breadcrumb.propTypes = {
   subtitle: PropTypes.string,
   items: PropTypes.array,
