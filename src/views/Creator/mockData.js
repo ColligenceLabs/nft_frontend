499e8ca4f6ff11ffe9fce3cf3b6c26b43cab103a
helloljho <helloljho@gmail.com>
2022-01-28 16:32:55 +0900

    fix : creator dummy data

---
+++ src/views/Creator/mockData.js
@@ -0,0 +1,150 @@
+export const rows = [
+  {
+    _id: {
+      $oid: '61cadd2a6279987120f2069f',
+    },
+    status: 'active',
+    name: 'DEV',
+    description: 'test',
+    image:
+      'https://nftbedev.talken.io/talkenNft/0xc005facf7de9673a464b5ff5dfce741a3f6e303a/company/1640684842784-talk_symbol01.png',
+    contract_id: {
+      $oid: '61cacc8ddecb6faa04e7dd41',
+    },
+    createdAt: {
+      $date: '2021-12-28T09:47:22.795Z',
+    },
+    updatedAt: {
+      $date: '2021-12-28T09:47:22.795Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '61cae0f56279987120f2070a',
+    },
+    status: 'active',
+    name: 'AEYL MUSIC',
+    description: '애일뮤직',
+    image:
+      'https://nftbedev.talken.io/talkenNft/0xc005facf7de9673a464b5ff5dfce741a3f6e303a/company/1640685813972-imgbin-bitcoin-cryptocurrency-computer-icons-bitcoin-s88gnYNpMF09zXNdaggRxQ1i6.jpg',
+    contract_id: {
+      $oid: '61cacc8ddecb6faa04e7dd41',
+    },
+    createdAt: {
+      $date: '2021-12-28T10:03:33.979Z',
+    },
+    updatedAt: {
+      $date: '2021-12-28T10:03:33.979Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '61cbc0ca6279987120f21da3',
+    },
+    status: 'active',
+    name: 'KOBEDOT',
+    description: 'https://koreabeatbox.com',
+    image:
+      'https://nftbedev.talken.io/talkenNft/0xc005facf7de9673a464b5ff5dfce741a3f6e303a/company/1640743114169-Contents Provider Symbol.jpg',
+    contract_id: {
+      $oid: '61cacc8ddecb6faa04e7dd41',
+    },
+    createdAt: {
+      $date: '2021-12-29T01:58:34.173Z',
+    },
+    updatedAt: {
+      $date: '2021-12-29T01:58:34.173Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '61b6eb2942f5221b36768f6c',
+    },
+    status: 'active',
+    name: 'dev company',
+    description: 'test',
+    image: 'https://nftbedev.talken.io/talkenNft/company/1639377705758-talk_symbol01.png',
+    createdAt: {
+      $date: '2021-12-13T06:41:45.764Z',
+    },
+    updatedAt: {
+      $date: '2021-12-13T06:41:45.764Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '61dceddc6279987120f23fee',
+    },
+    status: 'active',
+    name: 'HSNFT',
+    description: '임혁순 바보',
+    image:
+      'https://nftbedev.talken.io/talkenNft/0xc005facf7de9673a464b5ff5dfce741a3f6e303a/company/1641868764383-aespa-2.jpg',
+    contract_id: {
+      $oid: '61cacc8ddecb6faa04e7dd41',
+    },
+    createdAt: {
+      $date: '2022-01-11T02:39:24.393Z',
+    },
+    updatedAt: {
+      $date: '2022-01-11T02:39:24.393Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '61dd03c46279987120f241c3',
+    },
+    status: 'active',
+    name: 'POCKETMON',
+    description: '포켓몬스터 모음',
+    image:
+      'https://nftbedev.talken.io/talkenNft/0xc005facf7de9673a464b5ff5dfce741a3f6e303a/company/1641874372615-images.jpg',
+    contract_id: {
+      $oid: '61cacc8ddecb6faa04e7dd41',
+    },
+    createdAt: {
+      $date: '2022-01-11T04:12:52.623Z',
+    },
+    updatedAt: {
+      $date: '2022-01-11T04:12:52.623Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '61de9b76fa8768f97e21ebcb',
+    },
+    status: 'active',
+    name: 'test',
+    description: 'test',
+    image:
+      'https://nftbedev.talken.io/talkenNft/0xc005facf7de9673a464b5ff5dfce741a3f6e303a/company/test.png',
+    contract_id: {
+      $oid: '61cacc8ddecb6faa04e7dd41',
+    },
+    createdAt: {
+      $date: '2022-01-12T09:12:22.821Z',
+    },
+    updatedAt: {
+      $date: '2022-01-12T09:12:22.821Z',
+    },
+  },
+  {
+    _id: {
+      $oid: '61de9cebe59d4bfa38ba6725',
+    },
+    status: 'active',
+    name: 'TEST COMPANY',
+    description: 'test',
+    image:
+      'https://nftbedev.talken.io/talkenNft/0xc005facf7de9673a464b5ff5dfce741a3f6e303a/company/TEST COMPANY.jpeg',
+    contract_id: {
+      $oid: '61cacc8ddecb6faa04e7dd41',
+    },
+    createdAt: {
+      $date: '2022-01-12T09:18:35.440Z',
+    },
+    updatedAt: {
+      $date: '2022-01-12T09:18:35.440Z',
+    },
+  },
+];
---
+++ src/views/Creator/tableConfig.js
@@ -0,0 +1,26 @@
+export const headCells = [
+  {
+    id: 'name',
+    numeric: false,
+    disablePadding: false,
+    label: 'Name',
+  },
+  {
+    id: 'status',
+    numeric: false,
+    disablePadding: false,
+    label: 'Status',
+  },
+  {
+    id: 'createdAt',
+    numeric: false,
+    disablePadding: false,
+    label: 'Created at',
+  },
+  {
+    id: 'actions',
+    numeric: false,
+    disablePadding: false,
+    label: 'Actions',
+  },
+];
