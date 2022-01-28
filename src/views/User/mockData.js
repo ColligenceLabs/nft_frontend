a2e030d935aec9e1d5e6cc3615f339c629f2060a
helloljho <helloljho@gmail.com>
2022-01-28 16:59:49 +0900

    fix : users dummy data

---
+++ src/views/User/mockData.js
@@ -0,0 +1,162 @@
+export const rows = [
+  {
+    _id: {
+      $oid: '61b59c13af06080b8a7aa0b8',
+    },
+    address: null,
+    uid: 'wLdOowqA',
+    status: 'active',
+    tp_amount: 0,
+    createdAt: {
+      $date: '2021-12-12T06:52:03.696Z',
+    },
+    updatedAt: {
+      $date: '2021-12-12T06:52:03.696Z',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '61b59e53af06080b8a7aa0eb',
+    },
+    address: null,
+    uid: 'tuZwDA3u',
+    status: 'active',
+    tp_amount: 0,
+    createdAt: {
+      $date: '2021-12-12T07:01:39.231Z',
+    },
+    updatedAt: {
+      $date: '2021-12-12T07:01:39.231Z',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '61b5b0f1275f260e293d6bdb',
+    },
+    address: null,
+    uid: 'WOhZ4nHm',
+    status: 'active',
+    tp_amount: 0,
+    createdAt: {
+      $date: '2021-12-12T08:21:05.208Z',
+    },
+    updatedAt: {
+      $date: '2021-12-12T08:21:05.208Z',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '61b5b2fba80c7f0ea25112f7',
+    },
+    address: null,
+    uid: 'M3WzZgn5',
+    status: 'active',
+    tp_amount: 0,
+    createdAt: {
+      $date: '2021-12-12T08:29:47.700Z',
+    },
+    updatedAt: {
+      $date: '2021-12-12T08:29:47.700Z',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '61b7142542f5221b36768fdc',
+    },
+    address: null,
+    uid: '9v4R6Tbd',
+    status: 'active',
+    tp_amount: 0,
+    createdAt: {
+      $date: '2021-12-13T09:36:37.762Z',
+    },
+    updatedAt: {
+      $date: '2021-12-13T09:36:37.762Z',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '61ca6fc50004484790afcb05',
+    },
+    address: null,
+    uid: '6oFbRNqG',
+    status: 'active',
+    tp_amount: 0,
+    createdAt: {
+      $date: '2021-12-28T02:00:37.324Z',
+    },
+    updatedAt: {
+      $date: '2021-12-28T02:00:37.324Z',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '61cc41056279987120f2258b',
+    },
+    address: null,
+    uid: 'C8QQHXIG',
+    status: 'active',
+    tp_amount: 0,
+    createdAt: {
+      $date: '2021-12-29T11:05:41.597Z',
+    },
+    updatedAt: {
+      $date: '2021-12-29T11:05:41.597Z',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '61dcf0f76279987120f24157',
+    },
+    address: null,
+    uid: 'me7RXb5T',
+    status: 'active',
+    tp_amount: 0,
+    createdAt: {
+      $date: '2022-01-11T02:52:39.713Z',
+    },
+    updatedAt: {
+      $date: '2022-01-11T02:52:39.713Z',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '61de804fbbcbd4f3c48bf3cc',
+    },
+    address: null,
+    uid: '7Mvbqzz9',
+    status: 'active',
+    tp_amount: 0,
+    createdAt: {
+      $date: '2022-01-12T07:16:31.301Z',
+    },
+    updatedAt: {
+      $date: '2022-01-12T07:16:31.301Z',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '61e52d2ef77a7ffb39dafb1b',
+    },
+    address: null,
+    uid: 'tsPMqOLm',
+    status: 'active',
+    tp_amount: 0,
+    createdAt: {
+      $date: '2022-01-17T08:47:42.991Z',
+    },
+    updatedAt: {
+      $date: '2022-01-17T08:47:42.991Z',
+    },
+    __v: 0,
+  },
+];
---
+++ src/views/User/tableConfig.js
@@ -0,0 +1,26 @@
+export const headCells = [
+  {
+    id: 'uid',
+    numeric: false,
+    disablePadding: false,
+    label: 'UID',
+  },
+  {
+    id: 'status',
+    numeric: false,
+    disablePadding: false,
+    label: 'Status',
+  },
+  {
+    id: 'createdAt',
+    numeric: false,
+    disablePadding: false,
+    label: 'Created at',
+  },
+  {
+    id: 'actions',
+    numeric: false,
+    disablePadding: false,
+    label: 'Actions',
+  },
+];
