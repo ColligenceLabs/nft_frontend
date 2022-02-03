a9d1dfec20dc5d86233f99ca371732019d4eb640
helloljho <helloljho@gmail.com>
2022-02-03 15:14:47 +0900

    fix : logo & favicon

--- src/layouts/full-layout/logo/LogoIcon.js
+++ src/layouts/full-layout/logo/LogoIcon.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { ReactComponent as LogoDark } from '../../../assets/images/logos/logo-dark.svg';
-import { ReactComponent as LogoLight } from '../../../assets/images/logos/logo-white.svg';
+import TalkenLogo from '../../../assets/images/logos/talken_logo.png';
+import { Box } from '@mui/material';
 
 const LogoIcon = () => {
   const customizer = useSelector((state) => state.CustomizerReducer);
   return (
-    <Link underline="none" to="/">
-      {customizer.activeMode === 'dark' ? <LogoLight /> : <LogoDark />}
-    </Link>
+    <>
+      <Link underline="none" to="/">
+        <Box sx={{ padding: 1 }}>
+          {customizer.activeMode === 'dark' ? (
+            <img src={TalkenLogo} width="100px" alt="logo" />
+          ) : (
+            <img src={TalkenLogo} width="100px" alt="logo" />
+          )}
+        </Box>
+      </Link>
+    </>
   );
 };
 
