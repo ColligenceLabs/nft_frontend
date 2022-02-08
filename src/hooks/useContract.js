426ca13aa7b4932c2a9dff467f604ce1fe39693f
Peter Nahm <hwnahm@gmail.com>
2022-02-08 19:31:56 +0900

    feat : mint per collection

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -5,12 +5,19 @@ import kip17Abi from '../config/abi/kip17.json';
 import kip37Abi from '../config/abi/kip37.json';
 import useActiveWeb3React from './useActiveWeb3React';
 
-export const useKip17Contract = () => {
-  const { library } = useActiveWeb3React()
-  return useMemo(() => new ethers.Contract(contracts.kip17[1001], kip17Abi, library?.getSigner()), [library]);
-}
-
-export const useKip37Contract = () => {
+export const useKip17Contract = (contract) => {
   const { library } = useActiveWeb3React();
-  return useMemo(() => new ethers.Contract(contracts.kip37[1001], kip37Abi, library.getSigner()), [library]);
-}
+  // TODO: mint 할 collection 정보로 부터 contract address 를 보내줌.
+  // return useMemo(() => new ethers.Contract(contracts.kip17[1001], kip17Abi, library?.getSigner()), [library]);
+  return useMemo(() => new ethers.Contract(contract, kip17Abi, library?.getSigner()), [library]);
+};
+
+export const useKip37Contract = (contract) => {
+  const { library } = useActiveWeb3React();
+  // TODO: mint 할 collection 정보로 부터 contract address 를 보내줌.
+  // return useMemo(
+  //   () => new ethers.Contract(contracts.kip37[1001], kip37Abi, library.getSigner()),
+  //   [library],
+  // );
+  return useMemo(() => new ethers.Contract(contract, kip37Abi, library.getSigner()), [library]);
+};
