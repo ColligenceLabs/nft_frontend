c4b7827cde83136d529b9cf96855cbc1f0430c28
helloljho <helloljho@gmail.com>
2022-02-10 14:23:11 +0900

    fix : add creator list hook

---
+++ src/hooks/useCreator.js
@@ -0,0 +1,26 @@
+import { getCreatorData } from '../services/creator.service';
+import { useEffect, useState } from 'react';
+
+const useCreator = () => {
+  const [creatorList, setCreatorList] = useState();
+
+  useEffect(() => {
+    const fetchCreator = async () => {
+      await getCreatorData()
+        .then(({ data: { items } }) => {
+          console.log(items);
+          let creatorArray = items.map((item) => ({
+            _id: item._id,
+            full_name: item.full_name,
+          }));
+          setCreatorList(creatorArray);
+        })
+        .catch((error) => console.log(error));
+    };
+    fetchCreator();
+  }, [getCreatorData]);
+
+  return creatorList;
+};
+
+export default useCreator;
