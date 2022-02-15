952e30c8481718b861efb59c60f8fdf02d92a965
helloljho <helloljho@gmail.com>
2022-02-15 13:36:37 +0900

    fix : useUserInfor hook bug

--- src/hooks/useUserInfo.js
+++ src/hooks/useUserInfo.js
@@ -1,26 +1,11 @@
 import { useSelector } from 'react-redux';
-import { useState, useEffect } from 'react';
 
 const useUserInfo = () => {
-  const { user } = useSelector((state) => state.auth);
-
-  const [id, setId] = useState('');
-  const [full_name, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [level, setLevel] = useState('');
-  const [image, setImage] = useState('');
-  const [description, setDescription] = useState('');
-
-  useEffect(() => {
-    if (user.infor !== undefined) {
-      setId(user.infor.id);
-      setFullName(user.infor.full_name);
-      setEmail(user.infor.email);
-      setLevel(user.infor.level);
-      setImage(user.infor.image);
-      setDescription(user.infor.description);
-    }
-  }, [user]);
+  const {
+    user: {
+      infor: { id, full_name, level, email, image, description },
+    },
+  } = useSelector((state) => state.auth);
 
   return { id, full_name, level, email, image, description };
 };
