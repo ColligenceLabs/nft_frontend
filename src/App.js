fa2f886e839fde4c98a8a8f8dfa568ef0f7ca78d
helloljho <helloljho@gmail.com>
2022-02-21 17:35:35 +0900

    fix : remove RLT

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { useSelector } from 'react-redux';
-import RTL from './layouts/full-layout/customizer/RTL';
 import ThemeSettings from './layouts/full-layout/customizer/ThemeSettings';
 import Router from './routes/Router';
 import 'react-perfect-scrollbar/dist/css/styles.css';
-import Login from './views/authentication/Login';
 
 const App = () => {
   const theme = ThemeSettings();
-  const customizer = useSelector((state) => state.CustomizerReducer);
   const { isLoggedIn } = useSelector((state) => state.auth);
   const routing = useRoutes(Router(isLoggedIn));
 
   return (
     <ThemeProvider theme={theme}>
-      <RTL direction={customizer.activeDir}>
-        <CssBaseline />
-        {routing}
-      </RTL>
+      <CssBaseline />
+      {routing}
     </ThemeProvider>
   );
 };
