af4c0231b520323c401bd70c8a9136ae5ef6bf74
helloljho <helloljho@gmail.com>
2022-02-28 13:42:52 +0900

    fix : add network selector in Header

---
+++ src/components/NetworkSelector/networks.js
@@ -0,0 +1,17 @@
+import klayLogo from '../../assets/images/network_icon/klaytn-klay-logo.png';
+import solLogo from '../../assets/images/network_icon/solana-sol-logo.png';
+
+const NETWORKS = [
+  {
+    value: 'klay',
+    label: 'Klaytn',
+    icon: klayLogo,
+  },
+  {
+    value: 'sol',
+    label: 'Solana',
+    icon: solLogo,
+  },
+];
+
+export default NETWORKS;
