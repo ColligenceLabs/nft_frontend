fc831707495381ec55fa2bce31f32eeb63c4377d
Peter Nahm <hwnahm@gmail.com>
2022-03-06 12:52:58 +0900

    feat: separate Solana functions

---
+++ src/views/Solana/initializeStore.ts
@@ -0,0 +1,19 @@
+import { useWallet } from '@solana/wallet-adapter-react';
+import { useConnection, WhitelistedCreator } from '@colligence/metaplex-common';
+import { saveAdmin } from '../../solana/actions/saveAdmin';
+
+export const initializeStore = async () => {
+  const wallet = useWallet();
+  const connection = useConnection();
+
+  if (!wallet.publicKey) {
+    return;
+  }
+
+  await saveAdmin(connection, wallet, false, [
+    new WhitelistedCreator({
+      address: wallet.publicKey.toBase58(),
+      activated: true,
+    }),
+  ]);
+};
