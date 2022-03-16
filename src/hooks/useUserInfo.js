cb96214d1b11ac5203677c09613a5da01a8ad16b
helloljho <helloljho@gmail.com>
2022-03-16 17:15:58 +0900

    fix : change userUserInfo hook

--- src/hooks/useUserInfo.js
+++ src/hooks/useUserInfo.js
@@ -1,13 +1,20 @@
 import { useSelector } from 'react-redux';
 
 const useUserInfo = () => {
-  const {
-    user: {
-      infor: { id, full_name, level, email, image, description },
-    },
-  } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state?.auth);
 
-  return { id, full_name, level, email, image, description };
+  if (user !== null) {
+    return {
+      id: user.infor.id,
+      full_name: user.infor.full_name,
+      level: user.infor.level,
+      email: user.infor.email,
+      image: user.infor.image,
+      description: user.infor.description,
+    };
+  } else {
+    return { id: null, full_name: null, level: null, email: null, image: null, description: null };
+  }
 };
 
 export default useUserInfo;
