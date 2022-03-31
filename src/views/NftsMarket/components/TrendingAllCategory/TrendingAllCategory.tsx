398f2a8bf5dc2f0461df7559636b30938e2f4aef
helloljho <helloljho@gmail.com>
2022-03-31 15:18:02 +0900

    fix : remove unused files

--- src/views/NftsMarket/components/TrendingAllCategory/TrendingAllCategory.tsx
+++ src/views/NftsMarket/components/TrendingAllCategory/TrendingAllCategory.tsx
@@ -1,4 +1,6 @@
 import { Box, CardMedia, Typography, useTheme } from '@mui/material';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import React from 'react';
 import Slider from 'react-slick';
 import img1 from '../../../../assets/images/products/s1.jpg';
@@ -10,6 +12,20 @@ import img6 from '../../../../assets/images/products/s6.jpg';
 import img7 from '../../../../assets/images/products/s7.jpg';
 import img8 from '../../../../assets/images/products/s8.jpg';
 import useMediaQuery from '@mui/material/useMediaQuery';
+
+function Arrow(props: any) {
+  const theme = useTheme();
+  const { className, style, onClick, direction, color } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, color: theme.palette.primary.main }}
+      onClick={onClick}
+    >
+      {direction === 'next' ? <ArrowForwardIosIcon /> : <ArrowBackIosIcon />}
+    </div>
+  );
+}
 
 const TrendingAllCategory = () => {
   const theme = useTheme();
@@ -23,6 +39,8 @@ const TrendingAllCategory = () => {
     speed: 500,
     slidesToShow: mdDown ? 1 : 4,
     slidesToScroll: 1,
+    nextArrow: <Arrow direction="next" />,
+    prevArrow: <Arrow direction="prev" />,
   };
 
   return (
