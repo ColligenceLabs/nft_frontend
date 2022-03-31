31f2e517d114f232635ec93358b647a8d4b299f6
helloljho <helloljho@gmail.com>
2022-03-31 18:52:51 +0900

    fix : logo size in mobile layout

--- src/layouts/full-layout/logo/LogoIcon.js
+++ src/layouts/full-layout/logo/LogoIcon.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import TalkenLogo from '../../../assets/images/logos/talken_logo.png';
 import marketLogo from '../../../assets/images/logos/market-logo.svg';
 import { Box } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 const LogoIcon = () => {
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'), {
+    defaultMatches: true,
+  });
   const customizer = useSelector((state) => state.CustomizerReducer);
   return (
     <>
       <Link underline="none" to="/">
-        <Box sx={{ padding: 1 }}>
+        <Box sx={{ padding: 1, display: 'flex', alignItems: 'center' }}>
           {customizer.activeMode === 'dark' ? (
-            <img src={marketLogo} width="230px" alt="logo" />
+            <img src={marketLogo} width={smDown ? '180' : '230px'} alt="logo" />
           ) : (
-            <img src={marketLogo} width="230px" alt="logo" />
+            <img src={marketLogo} width={smDown ? '180' : '230px'} alt="logo" />
           )}
         </Box>
       </Link>
