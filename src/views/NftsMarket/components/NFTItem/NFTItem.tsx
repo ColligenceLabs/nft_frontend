808d261b64ef75545b482acd1bf597e2fb761574
helloljho <helloljho@gmail.com>
2022-04-08 16:00:59 +0900

    fix : show mp4 type icon

--- src/views/NftsMarket/components/NFTItem/NFTItem.tsx
+++ src/views/NftsMarket/components/NFTItem/NFTItem.tsx
@@ -4,9 +4,12 @@ import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { NFTType } from '../../types';
 import klayLogo from '../../../../assets/images/network_icon/klaytn-klay-logo.png';
+// @ts-ignore
+import FeatherIcon from 'feather-icons-react';
 
 const NFTItem: React.FC<NFTType> = ({ item }) => {
   const theme = useTheme();
+  console.log(item);
   return (
     <>
       <Link to={`/market/detail/${item._id}`} style={{ textDecoration: 'none' }}>
@@ -20,16 +23,49 @@ const NFTItem: React.FC<NFTType> = ({ item }) => {
             '&:hover': {
               transform: `translateY(-${theme.spacing(1 / 2)})`,
             },
+            zIndex: 80,
           }}
         >
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            {item?.metadata?.content_Type === 'mp4' && (
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'flex-end',
+                  width: '100%',
+                  zIndex: 1000,
+                  pt: 1.5,
+                  pr: 2,
+                }}
+              >
+                <Box
+                  sx={{
+                    border: '1px solid white',
+                    borderRadius: '50%',
+                    backgroundColor: 'gray',
+                    opacity: 0.6,
+                    width: '40px',
+                    height: '40px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  boxShadow={3}
+                >
+                  <FeatherIcon icon="video" height="24" color={'white'} />
+                </Box>
+              </Box>
+            )}
+
             <CardMedia
+              sx={{ mt: item?.metadata?.content_Type === 'mp4' ? '-52px' : '0px' }}
               component={'img'}
               height="270"
               image={item?.metadata?.thumbnail}
               alt={item?.metadata?.name}
             />
           </Box>
+
           <CardContent>
             <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
               <Box>
