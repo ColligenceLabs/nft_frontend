473abe1e46197ed0942322ae37c0fdf6df987311
helloljho <helloljho@gmail.com>
2022-04-13 11:49:44 +0900

    fix : market logo

--- src/layouts/full-layout/logo/LogoIcon.js
+++ src/layouts/full-layout/logo/LogoIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import marketLogo from '../../../assets/images/logos/market-logo.svg';
+import marketLogo from '../../../assets/images/logos/taalswap-marketplace_logo.png';
 import { Box } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
@@ -17,9 +17,9 @@ const LogoIcon = () => {
       <Link underline="none" to="/">
         <Box sx={{ padding: 1, display: 'flex', alignItems: 'center' }}>
           {customizer.activeMode === 'dark' ? (
-            <img src={marketLogo} width={smDown ? '180' : '230px'} alt="logo" />
+            <img src={marketLogo} width={smDown ? '130' : '150'} alt="logo" />
           ) : (
-            <img src={marketLogo} width={smDown ? '180' : '230px'} alt="logo" />
+            <img src={marketLogo} width={smDown ? '130' : '150'} alt="logo" />
           )}
         </Box>
       </Link>
