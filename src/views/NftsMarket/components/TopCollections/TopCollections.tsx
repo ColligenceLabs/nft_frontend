d72c81ee486a8b5be976521eecec053af9047b44
helloljho <helloljho@gmail.com>
2022-04-13 13:51:05 +0900

    fix : time category selector

--- src/views/NftsMarket/components/TopCollections/TopCollections.tsx
+++ src/views/NftsMarket/components/TopCollections/TopCollections.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
-import { Avatar, Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Avatar, Box, Button, Menu, MenuItem, Typography, Divider } from '@mui/material';
 import { mock } from './mock';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+interface CategoryTypes {
+  id: number;
+  value: string;
+  caption: string;
+}
+
+const CATEGORY: CategoryTypes[] = [
+  {
+    id: 0,
+    value: 'last24hours',
+    caption: 'last 24 hours',
+  },
+  {
+    id: 1,
+    value: 'last7days',
+    caption: 'last 7 days',
+  },
+  {
+    id: 2,
+    value: 'last30days',
+    caption: 'last 30 days',
+  },
+];
 
 const TopCollections = () => {
   const theme = useTheme();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [category, setCategory] = useState<CategoryTypes>(CATEGORY[1]);
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCategory = (selectedCategory: CategoryTypes) => {
+    setCategory(selectedCategory);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const isMd = useMediaQuery(theme.breakpoints.up('md'), {
     defaultMatches: true,
   });
@@ -34,9 +76,34 @@ const TopCollections = () => {
         <Typography fontSize={'30px'} fontWeight={'700'}>
           Top Collections over
         </Typography>
-        <Typography fontSize={'30px'} fontWeight={'700'} color={'primary'}>
-          last 7 days
-        </Typography>
+        <Button
+          id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+          sx={{ fontSize: '30px', fontWeight: '700', color: 'primary' }}
+        >
+          {category.caption}
+          <KeyboardArrowDownIcon />
+        </Button>
+
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          {CATEGORY.map((item, index) => (
+            <Box key={index} sx={{ minWidth: '200px' }}>
+              <MenuItem onClick={() => handleCategory(item)}>{item.caption}</MenuItem>
+              {index < CATEGORY.length - 1 && <Divider />}
+            </Box>
+          ))}
+        </Menu>
       </Box>
       <Box
         sx={{
