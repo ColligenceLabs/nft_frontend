85438beb14cd50adb04b0a40b63fe1b164331e70
seongjin1026 <seongjin.kim@owdin.network>
2022-05-11 14:05:40 +0900

    feat: add get nft sales count

--- src/services/serials.service.js
+++ src/services/serials.service.js
@@ -19,6 +19,19 @@ export const getSerialsData = (page, rowsPerPage, searchStatus, searchNftId) => 
     );
 };
 
+export const getNumberOfSales = (nftId, account) => {
+  console.log(nftId);
+  const url = `${API_URL}/sales-count/${nftId}?account=${account}`;
+  return axios
+    .get(url, { headers: authHeader() })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) =>
+      error.response.status === 401 ? (window.location.href = '/auth/login') : console.log(error),
+    );
+};
+
 export const setSerialsActive = (nftId, tokenId, quantity, pubkeys) => {
   return axios
     .put(
