2635697c7cb3390d441067619415f7a63ace3993
helloljho <helloljho@gmail.com>
2022-05-13 15:09:16 +0900

    fix : market address to Market string

--- src/utils/splitAddress.js
+++ src/utils/splitAddress.js
@@ -1,9 +1,10 @@
+import contracts from '../config/constants/contracts';
+
 const splitAddress = (str = '') => {
-  return str
-    ? str !== '0x0000000000000000000000000000000000000000'
-      ? str.substr(0, 5) + '...' + str.substr(str.length - 5, str.length)
-      : '-'
-    : '-';
+  const contract = contracts.marketV5[process.env.REACT_APP_MAINNET === 'true' ? 8217 : 1001];
+  if (str === '' || str === '0x0000000000000000000000000000000000000000') return '-';
+  if (str === contract) return 'Market';
+  return str.substr(0, 5) + '...' + str.substr(str.length - 5, str.length);
 };
 
 export default splitAddress;
