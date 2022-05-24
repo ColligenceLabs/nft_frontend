efad7f7ecf7c1712df1ec35ddccfdf56c75e731e
helloljho <helloljho@gmail.com>
2022-05-24 17:41:42 +0900

    fix : add binance wallet connector

--- src/components/WalletConnectorDialog/BinanceWallet.js
+++ src/components/WalletConnectorDialog/BinanceWallet.js
@@ -1,13 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Grid, Typography } from '@mui/material';
+import React from 'react';
+import { Box, Grid } from '@mui/material';
 import metamask_icon from '../../assets/images/wallet_icons/wallet_icon_metamask.png';
 import WalletCard from './WalletCard';
-import { useDispatch, useSelector } from 'react-redux';
-import { useWeb3React } from '@web3-react/core';
-import { injected, kaikas, walletconnect } from '../../connectors';
-import { setActivatingConnector } from '../../redux/slices/wallet';
-import { setKlaytn } from '../../redux/slices/wallets';
-import { loginWithAddress } from '../../redux/slices/auth';
 
 const BinanceWalletList = [
   {
@@ -19,24 +13,10 @@ const BinanceWalletList = [
 ];
 
 const BinanceWallet = ({ klaytn }) => {
-  const dispatch = useDispatch();
-  const context = useWeb3React();
-  const { activate, account, chainId } = context;
-
-  const { user } = useSelector((state) => state.auth);
-
   const handleWalletCardClick = async (wallet) => {
     console.log(wallet);
   };
 
-  useEffect(() => {
-    if (account !== undefined) {
-      if (user?.infor?.level !== 'administrator') {
-        dispatch(loginWithAddress({ address: account, chainId }));
-      }
-      // dispatch(loginWithAddress({ address: account, chainId }));
-    }
-  }, [account]);
   return (
     <Box style={{ backgroundColor: '#f2f2f2', borderRadius: '5px' }}>
       <Grid container>
